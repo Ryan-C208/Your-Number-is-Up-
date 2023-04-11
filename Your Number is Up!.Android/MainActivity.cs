05c477ef466a999584b6e561c89f22034ef1ba5e
Ryan Curry <rcurry2@ycp.edu>
2023-04-11 14:16:26 -0400

    Mostly Done, just need logic for moving blocks at later stages, and try to add music

--- Your Number is Up!.Android/MainActivity.cs
+++ Your Number is Up!.Android/MainActivity.cs
@@ -4,19 +4,37 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Android.Media;
+using System.Drawing.Printing;
+using Java.Lang.Annotation;
+using Android.Views.Animations;
 
 namespace Your_Number_is_Up_.Droid
 {
     [Activity(Label = "Your_Number_is_Up_", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+       // MediaPlayer player;
         protected override void OnCreate(Bundle savedInstanceState)
         {
+
             base.OnCreate(savedInstanceState);
+
+            
+          
+                        
+
+
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+            //need to get music to work later
+            //player = MediaPlayer.Create(this, Resource.Raw.SongForApp);
+            
+            //player.Start();
+
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
--- Your Number is Up!/MainPage.xaml.cs
+++ Your Number is Up!/MainPage.xaml.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Numerics;
+using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using static System.Net.Mime.MediaTypeNames;
+
 
 namespace Your_Number_is_Up_
 {
@@ -41,6 +45,7 @@ namespace Your_Number_is_Up_
         bool RestartTimer = false;
         //Controls total range of values that can be used for the main equation
         int Maxrange = 11;
+        int Minrange = 1;
         public MainPage()
         {
             InitializeComponent();
@@ -49,6 +54,7 @@ namespace Your_Number_is_Up_
             Initialize_values();
 
             
+
         }
 
 
@@ -70,7 +76,8 @@ namespace Your_Number_is_Up_
             {
                 lives--;
 
-                Display_Lives();
+                
+                Initialize_values();
                 Timer(_countSeconds);
                 return false;
 
@@ -115,14 +122,76 @@ namespace Your_Number_is_Up_
             Display_Lives();
             Level.Text = "Level: " + level.ToString();
             SecondOp.Text = SecondOperand.ToString();
+            //if level is less than 4, make every block have the same number 
+            if(level <= 4)
+            {
 
-            Addition.Text = UserNumber.ToString();
 
-            Multiplication.Text = UserNumber.ToString();
+                
+                Addition.Text = UserNumber.ToString();
 
-            Division.Text = UserNumber.ToString();
+                Multiplication.Text = UserNumber.ToString();
 
-            Subtraction.Text = UserNumber.ToString();
+                Division.Text = UserNumber.ToString();
+
+                Subtraction.Text = UserNumber.ToString();
+                
+            }
+
+            //if the level is greator than 4, the numbers within the blocks will now not all be the same, each will be an individual number.
+            else
+            {
+                //MoveBlocks();
+                int offset1 = new Random().Next(-5, 5);
+
+                int offset2 = new Random().Next(-5, 5);
+
+                int offset3 = new Random().Next(-5, 5);
+
+                if (Operation == ADDITION)
+                {
+                    Addition.Text = UserNumber.ToString();
+                    
+                    Multiplication.Text = (UserNumber + offset1).ToString();
+
+                    Division.Text = (UserNumber + offset2).ToString();
+
+                    Subtraction.Text = (UserNumber + offset3).ToString();
+                }
+                else if (Operation == SUBTRACTION)
+                {
+                    Subtraction.Text = UserNumber.ToString();
+
+                    Addition.Text = (UserNumber + offset1).ToString();
+
+                    Division.Text = (UserNumber + offset2).ToString();
+
+                    Multiplication.Text = (UserNumber + offset3).ToString();
+                }
+                else if (Operation == MULTIPLICATION)
+                {
+                    Multiplication.Text = UserNumber.ToString();
+
+                    Addition.Text = (UserNumber + offset1).ToString();
+
+                    Division.Text = (UserNumber + offset2).ToString();
+
+                    Subtraction.Text = (UserNumber + offset3).ToString();
+                }
+                else if (Operation == DIVISION)
+                {
+                    Division.Text = UserNumber.ToString();
+
+                    Multiplication.Text = (UserNumber + offset1).ToString();
+
+                    Addition.Text = (UserNumber + offset2).ToString();
+
+                    Subtraction.Text = (UserNumber + offset3).ToString();
+                }
+
+
+            }
+            
 
             
         }
@@ -145,32 +214,67 @@ namespace Your_Number_is_Up_
         //This function increments the level, decreasing the time, and increasing the range of numbers used in the equations
         public void Increment_Level()
         {
-            if(level > 4)
+
+            initial_score = score;
+            if (level > 4)
             {
                 //make other numbers random numbers within range of correct number except for correct block
                 //if you think its worth it, also make the block operations move positions at another level milestone
                 //also at higher levels dont divide number by itself, ensure that if it picks divison, it picks harder divison with a wider range of numbers
+
                 
                 level++;
             }
 
-            initial_score = score;
-            _countSeconds -= 2;
-            level++;
-            Maxrange += 5;
+            else
+            {
+                _countSeconds -= 1;
+                level++;
+                Maxrange += 5;
+                Minrange -= 5;
+            }
+            
 
         }
         public void GameDone()
         {
             Navigation.PushAsync(new GameOver());
         }
+
+        //need to still implement this to move blocks to unique new positions
+        /*
+        public void MoveBlocks()
+        {
+            MainGrid.Children.Remove(Addition);
+            MainGrid.Children.Add(Addition, 1, 1);
+        }
+        
+
+        */
+
+
+
+
+
+
         //Create equations display values to mainpage 
         public void EquationCreator()
         {
             //Generating random number that user will click for the equation
-            UserNumber = new Random().Next(1, Maxrange);
+            UserNumber = new Random().Next(Minrange, Maxrange);
             //Generating random number that will be used as the second operand in the equation 
-            SecondOperand = new Random().Next(1, Maxrange);
+            SecondOperand = new Random().Next(Minrange, Maxrange);
+
+
+            //need to ensure that SecondOperand is not zero
+            while(SecondOperand == 0)
+            {
+                SecondOperand = new Random().Next(Minrange, Maxrange);
+            }
+
+
+
+
             //Randomly generating operation that will be used on the two numbers to get the result
             Operation = new Random().Next(0, 4);
 
@@ -188,16 +292,22 @@ namespace Your_Number_is_Up_
             {
                 Result = UserNumber * SecondOperand;
             }
+
+
             else if (Operation == DIVISION)
             {
                 Result = UserNumber / SecondOperand;
+                
                 //If the result is not an integer
-                while(Result * SecondOperand != UserNumber)
-                {
-                    //This will decremnt SecondOperand until the Result becomes an integer
-                    SecondOperand -= 1;
-                    Result = UserNumber / SecondOperand;
-                }
+                while (Result * SecondOperand != UserNumber)
+                  {
+                     //This will decremnt SecondOperand until the Result becomes an integer
+                     SecondOperand -= 1;
+                     Result = UserNumber / SecondOperand;
+                  }
+                
+                
+                
                 
             }
 
